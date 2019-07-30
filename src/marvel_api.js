f0b33215153db70588fab33a39db311dbe71e90a
karenhlai <laikarenh@gmail.com>
2019-07-29 21:46:11 -0700

    Add char to events

--- src/marvel_api.js
+++ src/marvel_api.js
@@ -9,32 +9,23 @@ let hash = CryptoJS.MD5(`${key.ts}${key.privateKey}${key.publicKey}`);
 let myData = [];
 
 function filterEvents(json) {
-  Object.values(json.data.results).forEach((event, idx) => 
+  Object.values(json.data.results).forEach((event) => {
     myData.push(
       {
         ['name']: event.title, 
         // ['description']: event.description,
-        ['children']: [],
-        ['thumbnails']: event.thumbnail
+        ['children']: [event.characters.items],
+        // ['thumbnails']: event.thumbnail
       }
     )
-  );
+  });
   return myData;
-};
-
-function appendCharactersToEvents(json) {
-  Object.values(json.data.results).forEach((event) => {
-    // console.log(event.characters.items)
-    event.characters.items.forEach(characters =>
-      console.log(characters.name)
-    )
-  })
 };
 
 
 fetch(`http://gateway.marvel.com/v1/public/events?limit=1&ts=${key.ts}&apikey=${key.publicKey}&hash=${hash}`)
   .then(res => res.json())
-  // .then(myJson => filterEvents(myJson))
-  .then(myJson => appendCharactersToEvents(myJson))
+  .then(myJson => filterEvents(myJson))
+  // .then(myJson => appendCharactersToEvents(myJson))
   .catch(err => console.log(err));
 
