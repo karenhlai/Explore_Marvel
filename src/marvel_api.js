97dbeec3d69156931dd3797a9673564e603532c2
karenhlai <laikarenh@gmail.com>
2019-07-29 21:11:46 -0700

    Add children array

--- src/marvel_api.js
+++ src/marvel_api.js
@@ -2,32 +2,32 @@ var CryptoJS = require("crypto-js");
 const fetch = require('node-fetch');
 // import key from '../config/dev_keys';
 
+key = {
+  publicKey: '5bb20da87b09c9516bd86c9c5d2bcdad',
+  privateKey: '54bb3c8fc2734c8a92ca99cd532c76bb63a85c24',
+  ts: Date.now()
+};
 
 let hash = CryptoJS.MD5(`${key.ts}${key.privateKey}${key.publicKey}`);
 let myData = [];
 
 function filterEvents(json) {
-  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-
-  Object.values(json.data.results).forEach((event, idx) => {
+  Object.values(json.data.results).forEach((event, idx) => 
     myData.push(
       {
-        ['id']: idx, 
-        ['title']: event.title, 
-        ['description']: event.description
+        ['name']: event.title, 
+        // ['description']: event.description,
+        ['children']: [],
       }
-    );
-    // event.characters.items.forEach((character, jdx) => {
-    //   myData.nodes.push(
-    //     {
-    //       ['charId']: jdx, 
-    //       ['title']: character.name
-    //     }
-    //   );
-    // })
-  });
+    )
+  );
   return myData;
 };
+
+function appendCharactersToEvents(json) {
+  
+}
+
 
 fetch(`http://gateway.marvel.com/v1/public/events?limit=3&ts=${key.ts}&apikey=${key.publicKey}&hash=${hash}`)
   .then(res => res.json())
