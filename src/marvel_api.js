c3b452d7d5a9e7ee67729fa60e551dcc5805837f
karenhlai <laikarenh@gmail.com>
2019-07-29 21:13:24 -0700

    Add children array

--- src/marvel_api.js
+++ src/marvel_api.js
@@ -3,31 +3,27 @@ const fetch = require('node-fetch');
 // import key from '../config/dev_keys';
 
 
+
 let hash = CryptoJS.MD5(`${key.ts}${key.privateKey}${key.publicKey}`);
 let myData = [];
 
 function filterEvents(json) {
-  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-
-  Object.values(json.data.results).forEach((event, idx) => {
+  Object.values(json.data.results).forEach((event, idx) => 
     myData.push(
       {
-        ['id']: idx, 
-        ['title']: event.title, 
-        ['description']: event.description
+        ['name']: event.title, 
+        // ['description']: event.description,
+        ['children']: [],
       }
-    );
-    // event.characters.items.forEach((character, jdx) => {
-    //   myData.nodes.push(
-    //     {
-    //       ['charId']: jdx, 
-    //       ['title']: character.name
-    //     }
-    //   );
-    // })
-  });
+    )
+  );
   return myData;
 };
+
+function appendCharactersToEvents(json) {
+  
+}
+
 
 fetch(`http://gateway.marvel.com/v1/public/events?limit=3&ts=${key.ts}&apikey=${key.publicKey}&hash=${hash}`)
   .then(res => res.json())
